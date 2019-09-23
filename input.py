e5521cdb297bc0b4378451b9c690138782d1ff7c
Glen Liu <gliu@vermeg.com>
2019-09-23 17:47:37 +0800

    input

---
+++ input.py
@@ -0,0 +1,12 @@
+﻿def Text_1_answer(text, answer):
+    print(text)
+    while True:
+        space = input("Input answer:")
+        if space != answer:
+            print("Wrong, input again!")
+        else:
+            print("Yes go!")
+            break;
+
+
+Text_1_answer("test","abc")
